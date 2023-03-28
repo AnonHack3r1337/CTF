4f29ededd91631f9d169b0b2bcba181cd394698a
AnonHack3r1337 <129129988+AnonHack3r1337@users.noreply.github.com>
2023-03-27 21:59:49 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<select id="toc-dropdown">
+  <option value="">Table of Contents</option>
+  <option value="#introduction">Introduction</option>
+  <option value="#background">Background</option>
+  <option value="#methodology">Methodology</option>
+  <option value="#results">Results</option>
+  <option value="#conclusion">Conclusion</option>
+</select>
