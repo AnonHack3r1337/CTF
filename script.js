9810def1068f4c6332f226def83dd168b46b012a
AnonHack3r <129129988+AnonHack3r1337@users.noreply.github.com>
2023-04-02 13:05:43 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('toc-dropdown').addEventListener('change', function() {
-  var selected = this.value;
-  if (selected !== '') {
-    window.location.href = selected;
-  }
-});
